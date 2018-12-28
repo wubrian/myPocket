3002938e92bc567af34dfce80fdca890d45a551f
Abhi_G <devabhi.code@gmail.com>
2018-12-28 22:52:01 +0000

    Registration Page

--- server.js
+++ server.js
@@ -48,9 +48,13 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
 app.get("/login", (req, res) => {
-  res.render("login.ejs");
-})
+  res.render("login");
+});
 
 // MyResource page
 app.get("/myresource", (req, res) => {
