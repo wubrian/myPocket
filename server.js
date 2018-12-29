9c090ccdfe9461a48b06ae710e2948f158fcfdbc
Abhi_G <devabhi.code@gmail.com>
2018-12-29 23:40:36 +0000

    Registration functionality enabled. Cookies enabled.

--- server.js
+++ server.js
@@ -13,9 +13,13 @@ const knexConfig  = require("./knexfile");
 const knex        = require("knex")(knexConfig[ENV]);
 const morgan      = require('morgan');
 const knexLogger  = require('knex-logger');
+const cookieSession = require('cookie-session');
+const cookieParser = require('cookie-parser');
 
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
+
+app.use(cookieParser());
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -27,6 +31,14 @@ app.use(knexLogger(knex));
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2', 'key3'],
+  maxAge: 24 * 60 * 60 * 1000 //24 hour expiry..
+}));
+
+
 app.use("/styles", sass({
   src: __dirname + "/styles",
   dest: __dirname + "/public/styles",
@@ -52,8 +64,104 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
+function findUser(inputEmail){
+  knex.select('email', 'name')
+  .from('users')
+  .where('email', inputEmail)
+  .asCallback((err, result) => {
+    if(err) {console.log(err)};
+    console.log(result[0]);
+    return result[0];
+  });
+}
+
+app.post('/register', (req, res) => {
+  let email = req.body.email;
+  if(findUser(req.body.email)){
+    res.status(403).send("Another user is already registered with this email ID");  
+  } else if (req.body.email === '' || req.body.password === '' || req.body.name === '') {
+    res.send("Forms can't be left empty");
+  } else {
+      knex('users')
+      .insert({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      })
+      .asCallback((err) => {
+        if(err) {
+          console.log(err);
+        }
+        req.session.userCookie = email;
+        res.redirect('/');
+      })
+  } 
+
+});
+
 app.get("/login", (req, res) => {
   res.render("login");
+});
+
+// Search request
+app.post("/search", (req, res) => {
+  const searchText = req.body.search;
+  console.log(searchText)
+  knex.select('title','description','image','users.name','categories.category','email','password')
+  .from('urls')
+  .leftJoin('categories','urls.category_id', 'categories.id')
+  .leftJoin('users','urls.user_id', 'users.id')
+  .where('title', 'like', `%${searchText}%`)
+  .orWhere('description', 'like', `%${searchText}%`)
+  .asCallback((err,event) => {
+    if (err) throw err; 
+     console.log(event);
+     res.render("index");
+  })
+});
+
+//engineering category
+app.get("/engineering", (req,res) => {
+  knex.select('title','description','image','users.name','categories.category','email','password')
+  .from('urls')
+  .leftJoin('categories','urls.category_id', 'categories.id')
+  .leftJoin('users','urls.user_id', 'users.id')
+  .where('category', 'like', 'Engineering')
+  .asCallback((err,event) => {
+    if (err) throw err;
+     console.log(event);
+
+     res.render("engineering");
+  })
+});
+
+//web development category
+app.get("/webDev", (req,res) => {
+  knex.select('title','description','image','users.name','categories.category','email','password')
+  .from('urls')
+  .leftJoin('categories','urls.category_id', 'categories.id')
+  .leftJoin('users','urls.user_id', 'users.id')
+  .where('category', 'like', 'Web Dev')
+  .asCallback((err,event) => {
+    if (err) throw err;
+     console.log(event);
+     res.render("webDev");
+  })
+});
+
+//computer science category
+app.get("/cs", (req,res) => {
+  knex.select('title','description','image','users.name','categories.category','email','password')
+  .from('urls')
+  .leftJoin('categories','urls.category_id', 'categories.id')
+  .leftJoin('users','urls.user_id', 'users.id')
+  .where('category', 'like', 'CS')
+  .asCallback((err,event) => {
+    if (err) throw err;
+     console.log(event);
+     res.render("cs")
+
+  })
 });
 
 // MyResource page
